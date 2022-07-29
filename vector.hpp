5d75f71ec001642cf3f20a3e80a8537691fb0f13
mansur0987-del <85795437+mansur0987-del@users.noreply.github.com>
2022-07-29 18:41:24 +0300

    main

--- vector.hpp
+++ vector.hpp
@@ -1,4 +1,27 @@
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
 
+# include <memory>
+
+namespace ft
+{
+	template <class T, class Alloc = std::allocator<T>>
+	class vector
+	{
+	public:
+		typedef T						value_type;
+		typedef Alloc					allocator_type;
+		typedef size_t					size_type;
+
+	private:
+		value_type*				_data;
+		allocator_type			_alloc;
+		size_type				_size;
+		size_type				_capacity;
+	public:
+
+	};
+}
+
+
 #endif
